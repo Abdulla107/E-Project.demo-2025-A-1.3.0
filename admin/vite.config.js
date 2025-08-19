5a304bfcbbd26830fce413bbaec6896f35f89959
Abdulla107 <abdullahilkafi2023@gmail.com>
2025-08-19 20:50:51 +0600

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
+    react(),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1600, // 1.6MB 
+  },
 })
