cb54b9dc2f0b72f90fbee8b95146505af2c7e5ae
Abdulla107 <abdullahilkafi2023@gmail.com>
2025-08-19 21:01:56 +0600

    Update api.js

--- admin/src/api/api.js
+++ admin/src/api/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const local = 'http://localhost:5000';
-const production = 'https://e-project-demo-2025-a-1-server.onrender.com'
+const production = 'https://e-project-demo-2025-a-1-3-0.onrender.com'
 
 const api = axios.create({
-    baseURL: `${local}/api`,
+    baseURL: `${production}/api`,
     withCredentials: true
 })
 
